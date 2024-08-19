655fcee20d6933965f3d8fa70614349c7ccb946f
shendyeff <shendyteuku2@gmail.com>
2024-08-19 15:32:14 +0700

    Update dashboard layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -95,19 +95,6 @@
             </svg>
         </x-widget>
     </div>
-    <!-- <div class="col-12">
-        @if ($orders->count() == 0)
-        <div class="alert alert-info d-flex align-items-center" role="alert">
-            <i class="fas fa-info-circle mr-2 fa-lg"></i>
-            Saat ini belum ada permintaan barang
-        </div>
-        @else
-        <div class="alert alert-danger d-flex align-items-center" role="alert">
-            <i class="fas fa-info-circle mr-2 fa-lg"></i>
-            Saat ini terdapat {{ $orders->count() }} permintaan barang menunggu konfirmasi.
-            <a href="" class="ml-1">Lihat Detail Permintaan</a>
-        </div>
-        @endif -->
     </div>
     <div class="row">
         <div class="col-12 col-lg-6">
@@ -142,54 +129,54 @@
 
 @push('js')
 <script>
-// @formatter:off
-document.addEventListener("DOMContentLoaded", function() {
-    window.ApexCharts && (new ApexCharts(document.getElementById('chart-total-sales'), {
-        chart: {
-            type: "donut",
-            fontFamily: 'inherit',
-            height: 400,
-            sparkline: {
-                enabled: true
+    // @formatter:off
+    document.addEventListener("DOMContentLoaded", function() {
+        window.ApexCharts && (new ApexCharts(document.getElementById('chart-total-sales'), {
+            chart: {
+                type: "donut",
+                fontFamily: 'inherit',
+                height: 400,
+                sparkline: {
+                    enabled: true
+                },
+                animations: {
+                    enabled: true
+                },
+            },
+            fill: {
+                opacity: 1,
+            },
+            series: @json($total),
+            labels: @json($label),
+            grid: {
+                strokeDashArray: 4,
+            },
+            colors: ["#206bc4", "#79a6dc", "#bfe399", "#7891b3", "#2596be"],
+            legend: {
+                show: true,
+                position: 'top'
+            },
+            tooltip: {
+                fillSeriesColor: true
+            },
+            dataLabels: {
+                enabled: true,
             },
             animations: {
-                enabled: true
-            },
-        },
-        fill: {
-            opacity: 1,
-        },
-        series: @json($total),
-        labels: @json($label),
-        grid: {
-            strokeDashArray: 4,
-        },
-        colors: ["#206bc4", "#79a6dc", "#bfe399", "#7891b3", "#2596be"],
-        legend: {
-            show: true,
-            position: 'top'
-        },
-        tooltip: {
-            fillSeriesColor: true
-        },
-        dataLabels: {
-            enabled: true,
-        },
-        animations: {
-            enabled: true,
-            easing: 'easeinout',
-            speed: 800,
-            animateGradually: {
                 enabled: true,
-                delay: 150
-            },
-            dynamicAnimation: {
-                enabled: true,
-                speed: 350
+                easing: 'easeinout',
+                speed: 800,
+                animateGradually: {
+                    enabled: true,
+                    delay: 150
+                },
+                dynamicAnimation: {
+                    enabled: true,
+                    speed: 350
+                }
             }
-        }
-    })).render();
-});
-// @formatter:on
+        })).render();
+    });
+    // @formatter:on
 </script>
 @endpush

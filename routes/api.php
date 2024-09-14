329ac0211bfd6562bcb07fae1a586338e03b9ad9
shendyeff <shendyteuku2@gmail.com>
2024-09-14 08:51:55 +0700

    chore: Refactor product search functionality in ProductController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TransactionController;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
